2cf80079463c24d1c8de77e705d0f10affd65b56
hafidh7 <hafidh_170798@yahoo.co.id>
2023-06-19 01:26:35 +0700

    update https, telegram call mp3 play, debug

--- src/Callmebot_ESP8266.cpp
+++ src/Callmebot_ESP8266.cpp
@@ -15,79 +15,131 @@
 Callmebot_ESP8266 Callmebot;
 
 /**
- * @brief http client
+ * converts error code to String
+ * @param error int
+ * @return String
+ */
+String Callmebot_ESP8266::errorToString(int error)
+{
+    switch(error) {
+    case HTTPC_ERROR_CONNECTION_FAILED:
+        return F("connection failed");
+    case HTTPC_ERROR_SEND_HEADER_FAILED:
+        return F("send header failed");
+    case HTTPC_ERROR_SEND_PAYLOAD_FAILED:
+        return F("send payload failed");
+    case HTTPC_ERROR_NOT_CONNECTED:
+        return F("not connected");
+    case HTTPC_ERROR_CONNECTION_LOST:
+        return F("connection lost");
+    case HTTPC_ERROR_NO_STREAM:
+        return F("no stream");
+    case HTTPC_ERROR_NO_HTTP_SERVER:
+        return F("no HTTP server");
+    case HTTPC_ERROR_TOO_LESS_RAM:
+        return F("not enough ram");
+    case HTTPC_ERROR_ENCODING:
+        return F("Transfer-Encoding not supported");
+    case HTTPC_ERROR_STREAM_WRITE:
+        return F("Stream write error");
+    case HTTPC_ERROR_READ_TIMEOUT:
+        return F("read Timeout");
+    default:
+        return String();
+    }
+}
+
+/**
+ * @brief set clock (NTP)
  * 
- * @param url url callmebot
  */
-void Callmebot_ESP8266::callmebothttp(String url)
+void Callmebot_ESP8266::clock()
 {
-	// Data to send with HTTP POST
-	WiFiClient client;    
-	HTTPClient http;
-	http.begin(client, url);
-
-	// Specify content-type header
-	http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-	
-	// Send HTTP POST request
-	int httpResponseCode = http.POST(url);
-	if (httpResponseCode == 200)
-	{
-		Serial.print("Message sent successfully");
+	// Set time via NTP, as required for x.509 validation
+	configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+	time_t now = time(nullptr);
+	while (now < 8 * 3600 * 2) {
+		delay(500);
+		now = time(nullptr);
 	}
-	else
-	{
-		Serial.println("Error sending the message");
-		Serial.print("HTTP response code: ");
-		Serial.println(httpResponseCode);
-	}
-
-	http.end();
+	struct tm timeinfo;
+	gmtime_r(&now, &timeinfo);
 }
 
 /**
- * CallMeBot WhatsApp Messages
- * @param phoneNumber Indonesia +62, Example: "+62897461238"
- * @param apiKey "apiKey", to get apiKey : https://www.callmebot.com/blog/telegram-group-messages-api-easy/
- * @param message "textmessage"
+ * @brief Begin
+ * 
  */
-void Callmebot_ESP8266::whatsappMessage(String phoneNumber, String apiKey, String message)
+void Callmebot_ESP8266::begin()
 {
-	String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
-	
-	callmebothttp(url);
+	Callmebot.clock();
 }
 
 /**
- * CallMeBot Facebook Messages.
- * @param apiKey "apiKey", to get apiKey : https://www.callmebot.com/blog/telegram-group-messages-api-easy/
- * @param message "textmessage"
+ * @brief https
+ * 
+ * @param url 
  */
-void Callmebot_ESP8266::facebookMessage(String apiKey, String message)
+void Callmebot_ESP8266::callmebothttps(String url)
 {
-	String url = "http://api.callmebot.com/facebook/send.php?apikey=" + apiKey + "&text=" + urlEncode(message);
+	X509List cert(cert_ISRG_Root_X1);
+	WiFiClientSecure client;
+	client.setTrustAnchors(&cert);
+	HTTPClient https;
 
-	callmebothttp(url);
+	this->httpStatus = https.begin(client, host+url);
+
+	if (this->httpStatus)
+	{
+		// start connection and send HTTP header
+		this->httpResponseCode = https.GET();
+		https.end();
+	}
 }
 
 /**
  * CallMeBot WhatsApp Messages.
- * @param username "username"
- * @param message "textmessage"
+ * @param phoneNumber Indonesia +62, Example: "+62897461238".
+ * @param apiKey "apiKey", to get apiKey : https://www.callmebot.com/blog/free-api-whatsapp-messages/.
+ * @param message "textmessage".
+ */
+void Callmebot_ESP8266::whatsappMessage(String phoneNumber, String apiKey, String message)
+{
+	String url = "/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+	
+	this->callmebothttps(url);
+}
+
+/**
+ * CallMeBot Facebook Messages.
+ * @param apiKey "apiKey", to get apiKey : https://www.callmebot.com/blog/free-api-facebook-messenger/.
+ * @param message "textmessage".
+ */
+void Callmebot_ESP8266::facebookMessage(String apiKey, String message)
+{
+	String url = "/facebook/send.php?apikey=" + apiKey + "&text=" + urlEncode(message);
+
+	this->callmebothttps(url);
+}
+
+/**
+ * CallMeBot WhatsApp Messages.
+ * @param username username/phone. phone: Indonesia +62, Example: "+62897461238".
+ * @param message "textmessage".
  */
 void Callmebot_ESP8266::telegramMessage(String username, String message)
 {
-	String url = "http://api.callmebot.com/text.php?user=" + username + "&text=" + urlEncode(message);
+	String url = "/text.php?user=" + username + "&text=" + urlEncode(message);
 
-	callmebothttp(url);
+	this->callmebothttps(url);
 }
 
 /**
  * @brief CallMeBot Telegram Group Messages.
  * 
- * @param apiKey "apiKey", to get apiKey : https://www.callmebot.com/blog/telegram-group-messages-api-easy/
- * @param message "textmessage"
- * @param html_format true/false. default = false. true if you want bold text
+ * @param apiKey "apiKey", to get apiKey : https://www.callmebot.com/blog/telegram-group-messages-api-easy/.
+ * @param message "textmessage".
+ * @param html_format true/false. default = false. true if you want bold text.
  */
 void Callmebot_ESP8266::telegramGroup(String apiKey, String message, bool html_format)
 {
@@ -98,15 +150,15 @@ void Callmebot_ESP8266::telegramGroup(String apiKey, String message, bool html_f
 		html = "yes";
 	}
 
-	String url = "http://api.callmebot.com/telegram/group.php?apikey=" + apiKey + "&text=" + urlEncode(message) + "&html=" + html;
+	String url = "/telegram/group.php?apikey=" + apiKey + "&text=" + urlEncode(message) + "&html=" + html;
 	
-	callmebothttp(url);
+	this->callmebothttps(url);
 }
 
 /**
- * @brief CallMeBot Teelegram Call.
+ * @brief CallMeBot Telegram Call.
  * 
- * @param username username/phone. phone: Indonesia +62, Example: "+62897461238"
+ * @param username username/phone. phone: Indonesia +62, Example: "+62897461238".
  * @param message "textmessage".
  * @param language default = en-US-Neural2-J. if you want to change the voice, you can get voice name on https://cloud.google.com/text-to-speech/docs/voices
  * @param repeat default = 2.
@@ -119,7 +171,58 @@ void Callmebot_ESP8266::telegramGroup(String apiKey, String message, bool html_f
  */
 void Callmebot_ESP8266::telegramCall(String username, String message, String language, unsigned long repeat, String textcarbon, unsigned long timeout)
 {
-	String url = "http://api.callmebot.com/start.php?user=" + username + "&text=" + urlEncode(message) + "&lang=" + language + "&rpt=" + String(repeat) + "&cc=" + textcarbon + "&timeout=" + String(timeout);
+	String url = "/start.php?user=" + username + "&text=" + urlEncode(message) + "&lang=" + language + "&rpt=" + String(repeat) + "&cc=" + textcarbon + "&timeout=" + String(timeout);
 	
-	callmebothttp(url);
+	this->callmebothttps(url);
+}
+
+/**
+ * @brief play mp3 on telegram call
+ * 
+ * @param username username/phone. phone: Indonesia +62, Example: "+62897461238".
+ * @param mp3link mp3 file link.
+ */
+void Callmebot_ESP8266::telegramCallMp3(String username, String mp3link)
+{
+	String url = "/start.php?user=" + username + "&file=" + urlEncode(mp3link);
+
+	this->callmebothttps(url);
+}
+
+/**
+ * @brief show debug
+ * 
+ * @return httpResponse
+ */
+String Callmebot_ESP8266::debug()
+{
+	String Response;
+	if (this->httpStatus==true)
+	{
+		// httpResponseCode will be negative on error
+		if (this->httpResponseCode > 0)
+		{
+			if (this->httpResponseCode == HTTP_CODE_OK)
+			{
+				Response = "Call/Message Request sent successfully";
+			}
+			else
+			{
+				Response = "Call/Message Request failed\n";
+				Response += "[HTTPS] GET... code: ";
+				Response += String(this->httpResponseCode);
+			}
+		}
+		else
+		{
+			Response = "[HTTPS] GET... failed, error: ";
+			Response += this->errorToString(httpResponseCode).c_str();
+		}
+	}
+	else
+	{
+		Response = "[HTTPS] Unable to connect\n";
+		Response += "Check your connection";
+	}
+	return Response;
 }
--- src/Callmebot_ESP8266.h
+++ src/Callmebot_ESP8266.h
@@ -14,21 +14,33 @@
 #define Callmebot_ESP8266_h
 
 #include "Arduino.h"
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
 #include <UrlEncode.h>
+#include <certs.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
 
 class Callmebot_ESP8266
 {
 private:
-    void callmebothttp(String url);
+    const char* host = "https://api.callmebot.com";
+    const uint16_t port = 443;
+    bool httpStatus;
+    int httpResponseCode;
+    void clock();
+    void callmebothttps(String url);
+    String errorToString(int error);
 public:
+    void begin();
     void whatsappMessage(String phoneNumber, String apiKey, String message);
     void facebookMessage(String apiKey, String message);
     void telegramMessage(String username, String message);
     void telegramGroup(String apiKey, String message, bool html_format = false);
     void telegramCall(String username, String message, String language = "en-US-Neural2-J", unsigned long repeat = 2, String textcarbon = "yes", unsigned long timeout = 30);
+    void telegramCallMp3(String username, String mp3link);
+    String debug();
 };
 
 extern Callmebot_ESP8266 Callmebot;
+
 #endif
